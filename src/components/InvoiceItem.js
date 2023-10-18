0ceafa1b2f1f7fce43a1c84ddc7f06d923a08cf7
hanjarraess <hanjarraess@gmail.com>
2023-10-18 10:08:30 +0700

    create startup

---
+++ src/components/InvoiceItem.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { BiTrash } from "react-icons/bi";
+import EditableField from './EditableField';
+
+const InvoiceItem = ({ onItemizedItemEdit, currency, onRowDel, onRowAdd, items }) => {
+  const itemTable = items.map(item => (
+    <>
+      <ItemRow
+        onItemizedItemEdit={onItemizedItemEdit}
+        item={item}
+        onDelEvent={() => onRowDel(item)}
+        key={item.id}
+        currency={currency}
+      />
+    </>
+
+  ));
+
+  return (
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>ITEM</th>
+            <th>QTY</th>
+            <th>PRICE/RATE</th>
+            <th className="text-center">ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          {itemTable}
+        </tbody>
+      </Table>
+      <Button className="fw-bold" onClick={onRowAdd}>Add Item</Button>
+    </div>
+  );
+};
+
+const ItemRow = ({ onItemizedItemEdit, item, onDelEvent, currency }) => {
+  const handleDelEvent = () => {
+    onDelEvent(item);
+  };
+
+  return (
+    <tr>
+      <td style={{ width: '100%' }}>
+        <EditableField
+          onItemizedItemEdit={onItemizedItemEdit}
+          cellData={{
+            type: "text",
+            name: "name",
+            placeholder: "Item name",
+            value: item.name,
+            id: item.id,
+          }}
+        />
+        <EditableField
+          onItemizedItemEdit={onItemizedItemEdit}
+          cellData={{
+            type: "text",
+            name: "description",
+            placeholder: "Item description",
+            value: item.description,
+            id: item.id
+          }}
+        />
+      </td>
+      <td style={{ minWidth: '70px' }}>
+        <EditableField
+          onItemizedItemEdit={onItemizedItemEdit}
+          cellData={{
+            type: "number",
+            name: "quantity",
+            min: 1,
+            step: "1",
+            value: item.quantity,
+            id: item.id,
+          }}
+        />
+      </td>
+      <td style={{ minWidth: '200px' }}>
+        <EditableField
+          onItemizedItemEdit={onItemizedItemEdit}
+          cellData={{
+            leading: currency,
+            type: "number",
+            name: "price",
+            min: 1,
+            step: "0.01",
+            presicion: 2,
+            textAlign: "text-end",
+            value: item.price,
+            id: item.id,
+          }}
+        />
+      </td>
+      <td className="text-center" style={{ minWidth: '50px' }}>
+        <BiTrash
+          onClick={handleDelEvent}
+          style={{ height: '33px', width: '33px', padding: '7.5px' }}
+          className="text-white mt-1 btn btn-danger"
+        />
+      </td>
+    </tr>
+  );
+};
+
+export default InvoiceItem;
